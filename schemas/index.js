0e5eb6420dc03b72ed2d91cc6f8456e338625ae6
yungi2383 <g830201@nate.com>
2023-08-31 15:08:07 +0900

    안녕 잘 부탁해

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,3 @@
-// schemas/todo.schema.js
 import mongoose from "mongoose";
 
 const connect = () => {
@@ -6,19 +5,28 @@ const connect = () => {
     .connect(
       "mongodb+srv://yungiDB:dbsrlgd@cluster0.bpcqzfd.mongodb.net/?retryWrites=true&w=majority",
       {
+        dbName: "node_level1",
         useNewUrlParser: true,
         useUnifiedTopology: true,
         tls: true,
-
-        dbName: "node_level1",
       }
     )
-    .catch((err) => console.log(err))
-    .then(() => console.log("몽고디비 연결 성공"));
+    .then(() => {
+      console.log("몽고디비 연결 성공");
+    })
+    .catch((err) => {
+      console.error("몽고디비 연결 실패", err);
+    });
 };
+
 // 몽고디비 연결 에러 이벤트 리스너
 mongoose.connection.on("error", (err) => {
   console.error("몽고디비 연결 에러", err);
 });
 
 export default connect;
+
+조윤주(N반);
+Available;
+
+66;
